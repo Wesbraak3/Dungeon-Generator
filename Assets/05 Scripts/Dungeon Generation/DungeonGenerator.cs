ade9fc663255026991fdec17fd6430517d758e84
Wes te Braak <wesbraak3@hotmail.com>
2025-03-07 00:33:20 +0100

    started setup for splitting
    added an package for inspector buttons
    setup an button for splitting the rooms
    for easyer debugging later

--- Assets/05 Scripts/Dungeon Generation/DungeonGenerator.cs
+++ Assets/05 Scripts/Dungeon Generation/DungeonGenerator.cs
@@ -1,29 +1,67 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 namespace DungeonGeneration {
     public class DungeonGenerator : MonoBehaviour {
-        [SerializeField]
+        #region Room Settings
+
+        [Header("Room Settings")]
+        [DisableIf("Enabled"), SerializeField] 
+        private RectInt initialSize = new(0, 0, 100, 50);
+        [DisableIf("Enabled"), SerializeField] 
+        private int height = 5;
+        [DisableIf("Enabled"), SerializeField] 
+        private Color dungeonColor = Color.blue;
+
+        [HorizontalLine]
+        [Header("Room Debug")]
+        [ReadOnly, SerializeField] 
         private List<RectInt> rooms = new();
 
-        [SerializeField] private RectInt initialSize = new(0, 0, 100, 50);
-        [SerializeField] private int height = 10;
-        [SerializeField] private Color dungeonColor = Color.red;
+        [HorizontalLine]
+        [Header("Room manager")]
+        [ReadOnly, SerializeField] 
+        private int roomCount = 0;
+
+        #endregion
+
+        // For disabling the inspector room settings after start
+        public bool Enabled() => roomCount > 0;
 
         void Start() {
+        }
+        void Update() {
+        }
+
+        /// <summary>
+        /// Testing setup for manuel creating splitting etc..
+        /// Later use logic to automate the proces
+        /// </summary>
+        [Button(enabledMode: EButtonEnableMode.Playmode)]
+        private void CreateInitialRoom() {
+            // clear stored values
+            DebugDrawingBatcher.ClearCalls();
+            rooms.Clear();
+
+            // make so battcher accept list as input instead of objects?
             DebugDrawingBatcher.BatchCall(() => {
                 AlgorithmsUtils.DebugRectInt(initialSize, dungeonColor, height:height);
             });
 
             rooms.Add(initialSize);
+            roomCount = rooms.Count;
         }
 
+        [Button(enabledMode: EButtonEnableMode.Playmode)]
         private void SplitRooms() {
+            roomCount = rooms.Count;
             throw new NotImplementedException();
         }
 
-        void Update() {
+        private void OnDrawGizmos() {
+            if (rooms.Count == 0) DebugExtension.DebugBounds(new Bounds(new Vector3(initialSize.center.x, 0, initialSize.center.y), new Vector3(initialSize.width, height, initialSize.height)), dungeonColor);
         }
     }
 }
