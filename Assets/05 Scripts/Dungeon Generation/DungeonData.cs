924a88dcbc8c3e003f426d2b6973843ed0ee411d
Wes te Braak <wesbraak3@hotmail.com>
2025-04-27 00:35:51 +0200

    place prefabs for door, wall, floors and player

--- Assets/05 Scripts/Dungeon Generation/DungeonData.cs
+++ Assets/05 Scripts/Dungeon Generation/DungeonData.cs
@@ -9,7 +9,6 @@ namespace DungeonGeneration {
         private List<DoorData> DoorList = new();
 
         public void AddRoom(RoomData newRoom) => RoomList.Add(newRoom);
-
         public void AddDoor(DoorData newDoor) {
             RoomData roomA = newDoor.ConnectedRooms[0];
             RoomData roomB = newDoor.ConnectedRooms[1];
@@ -197,4 +196,72 @@ namespace DungeonGeneration {
             ConnectedRooms[1] = roomB;
         }
     }
+
+
+    public class GridData {
+        Dictionary<Vector3Int, PlacementData> placedObjects = new();
+
+        public void AddObjectAt(Vector3Int gridPosition,
+                                Vector2Int objectSize,
+                                int ID,
+                                GameObject placedObjectIndex
+            ) {
+            List<Vector3Int> positionToOccupy = CalculatePositions(gridPosition, objectSize);
+            PlacementData data = new(positionToOccupy, ID, placedObjectIndex);
+            foreach (var pos in positionToOccupy) {
+                if (placedObjects.ContainsKey(pos)) {
+                    throw new System.Exception($"Dictionary already contains this cell position {pos}");
+                }
+                placedObjects[pos] = data;
+            }
+        }
+
+        private List<Vector3Int> CalculatePositions(Vector3Int gridPosition, Vector2Int objectSize) {
+            List<Vector3Int> returnVal = new();
+            for (int x = 0; x < objectSize.x; x++) {
+                for (int y = 0; y < objectSize.y; y++) {
+                    returnVal.Add(gridPosition + new Vector3Int(x, 0, y));
+                }
+            }
+            return returnVal;
+        }
+
+        public bool CanPlaceObjectAt(Vector3Int gridPosition, Vector2Int objectSize) {
+            List<Vector3Int> positionToOccupy = CalculatePositions(gridPosition, objectSize);
+            foreach (var pos in positionToOccupy) {
+                if (placedObjects.ContainsKey(pos)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public GameObject GetRepresentationIndex(Vector3Int gridPosition) {
+            if (placedObjects.ContainsKey(gridPosition) == false) {
+                return null;
+            }
+            return placedObjects[gridPosition].PlaceObject;
+        }
+
+        public void RemoveObjectAt(Vector3Int gridPosition) {
+            foreach (var pos in placedObjects[gridPosition].occupiedPositions) {
+                placedObjects.Remove(pos);
+            }
+        }
+    }
+
+    public class PlacementData {
+        public List<Vector3Int> occupiedPositions;
+
+        public int ID { get; private set; }
+        public GameObject PlaceObject { get; private set; }
+
+        public PlacementData(List<Vector3Int> occupiedPositions, int iD, GameObject placeObject) {
+            this.occupiedPositions = occupiedPositions;
+            ID = iD;
+            PlaceObject = placeObject;
+        }
+    }
+
+
 }
